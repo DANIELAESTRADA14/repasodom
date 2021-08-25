bd7d530b35865b70340fdb0ad7f38fa98f1a981d
hbuitgu <Buitrago321752>
2021-08-25 09:21:28 -0500

    se hace boton regresar

--- controlador.js
+++ controlador.js
@@ -6,6 +6,13 @@ let boton=document.getElementById("boton");
 let foto1=document.getElementById("foto1");
 let audio1=document.getElementById("audio1");
 let titulo1=document.getElementById("titulo1");
+let foto2=document.getElementById("foto2");
+let audio2=document.getElementById("audio2");
+let titulo2=document.getElementById("titulo2");
+let foto3=document.getElementById("foto3");
+let audio3=document.getElementById("audio3");
+let titulo3=document.getElementById("titulo3");
+
 
 boton.addEventListener("click", perroGuardian); //estoy haciendo un callback
 
@@ -23,10 +30,23 @@ function perroGuardian(){
     audio3.src="audio/man.mp3"
 }
 
-let foto2=document.getElementById("foto2");
-let audio2=document.getElementById("audio2");
-let titulo2=document.getElementById("titulo2");
-let foto3=document.getElementById("foto3");
-let audio3=document.getElementById("audio3");
-let titulo3=document.getElementById("titulo3");
+
+
+
+let boton2=document.getElementById("boton2");
+
+boton2.addEventListener("click", Regresar);
+
+function Regresar(){
+    titulo1.textContent="That's What I Like"
+    foto1.src="img/like.jpg";
+    audio1.src="audio/like.mp3"
+    titulo2.textContent="Locked Out of Heaven"
+    foto2.src="img/heaven.jpg";
+    audio2.src="audio/heaven.mp3"
+    titulo3.textContent="Uptown Funk"
+    foto3.src="img/uptown.jpg";
+    audio3.src="audio/uptown.mp3"
+}
+
 
